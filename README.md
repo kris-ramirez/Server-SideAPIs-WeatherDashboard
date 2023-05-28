2abffcd381f88dea199142233e733162f6e8a32d
Kristian Ramirez <Kristian@sids-MacBook-Pro-2.local>
2023-05-27 19:01:10 -0500

    Saturday code. able to pull all weather info by city. still need other two components

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 
 used code from activity 13 from bootstrp week 05 for columns
+
+
+
+https://stackoverflow.com/questions/62376115/how-to-obtain-open-weather-api-date-time-from-city-being-fetched
+https://stackoverflow.com/questions/46216830/showing-correct-date-from-api
+https://stackoverflow.com/questions/11701311/logo-image-and-h1-heading-on-the-same-line
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var searchFormEl = document.querySelector('#search-form');
-
-
-
-
+var APIKey = "b934080f8bc9b135e4a6aea8ae865c1b";
+var humidityEl = document.querySelector("#humid");
+var windEl = document.querySelector('#wind');
+var tempEl = document.querySelector('#temp');
+var cityNameEl = document.querySelector('#cityName');
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
@@ -14,46 +15,53 @@ function handleSearchFormSubmit(event) {
         return;
     }
     localStorage.setItem('search-input', searchInputVal);
+    // Construct the weather API URL with latitude and longitude
+    var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=' + searchInputVal + '&appid=' + APIKey;
+    console.log(weatherApiUrl);
 
-// Construct the geocoding API URL
-var geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + encodeURIComponent(searchInputVal) + '&key=8363ff74233a9241814b580d1b9a21c5';
+    // Make the API request
+    fetch(weatherApiUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Unable to fetch weather data.');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            // Process the retrieved weather data
+            console.log(data);
+            getParams(data);
+            // Display or use the weather data as needed
+        })
+        .catch(function (error) {
+            console.error('Error:', error.message);
+        });
+}
+function getParams(weatherData) {
+    //Display city, date, & icon
+    var cityName = weatherData.city.name;
 
-// Make the geocoding API request
-fetch(geocodingUrl)
-    .then(function (response) {
-        if (!response.ok) {
-            throw new Error('Unable to fetch geocoding data.');
-        }
-        return response.json();
-    })
-    .then(function (data) {
-        // Extract latitude and longitude from the geocoding response
-        var latitude = data.results[0].geometry.lat;
-        var longitude = data.results[0].geometry.lng;
+    var currentDate = weatherData.list[0].dt_txt;
+    var [apiDate, apiTime] = currentDate.split(" ");
+    var [year, month, day] = apiDate.split('-');
+    var formattedDate = "(" + month + "/" + day + "/" + year + ")";
 
-        // Construct the weather API URL with latitude and longitude
-        var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=8363ff74233a9241814b580d1b9a21c5';
 
-        // Make the API request
-        fetch(weatherApiUrl)
-            .then(function (response) {
-                if (!response.ok) {
-                    throw new Error('Unable to fetch weather data.');
-                }
-                return response.json();
-            })
-            .then(function (data) {
-                // Process the retrieved weather data
-                console.log(data);
-                // Display or use the weather data as needed
-            })
-            .catch(function (error) {
-                console.error('Error:', error.message);
-            });
-    })
-    .catch(function (error) {
-        console.error('Error:', error.message);
-    });
+    var currentIcon = weatherData.list[0].weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/w/" + currentIcon + ".png";
+    var inconEl = document.createElement("img")
+    inconEl.src = iconUrl;
+    var containerEl = document.getElementById("icon");
+    containerEl.appendChild(inconEl);
+    cityNameEl.textContent = cityName + ' ' + formattedDate + " ";
+
+    //Display temp, humidity, and wind
+    var temp = weatherData.list[0].main.temp;
+    tempEl.textContent = 'Temp: ' + temp + 'F';
+    var humidity = weatherData.list[0].main.humidity;
+    humidityEl.textContent = 'Humidity: ' + humidity + "%";
+    var wind = weatherData.list[0].wind.speed;
+    windEl.textContent = 'Wind: ' + wind + " MPH";
 
 }
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
